f3e87dbc4fe4604a8a6b6046a4de56673d0b4f19
ayubxon <ayubxonqosimov60@gmail.com>
2025-11-07 20:38:11 +0500

    I delete web component

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,7 +1,7 @@
 import React from "react";
 // import components
 import AboutSection from "../components/AboutSection";
-import BenefitsSection from "../components/BenefitsSection";
+import BenefitsSection from "../components/BenefitSection";
 import FaqSection from "../components/FaqSection";
 
 const AboutUs = () => {
