88310abf43e74c9be0e774ecf151f0544114dbd1
ayubxon <ayubxonqosimov60@gmail.com>
2025-11-07 16:55:12 +0500

    I remove README.md

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+// import components
+import AboutUs from "./pages/AboutUs";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>eLearning</h1>
+      <AboutUs />
+    </div>
+  );
+}
+
+export default App;
