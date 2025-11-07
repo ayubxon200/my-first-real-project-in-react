c68786a0dc64069b477d3f8bc5f825154da3a641
ayubxon <ayubxonqosimov60@gmail.com>
2025-11-07 22:14:23 +0500

    Nav complete static styling

---
+++ src/components/Nav.js
@@ -0,0 +1,56 @@
+import React from "react";
+import styled from "styled-components";
+
+const Nav = () => {
+  return (
+    <StyledNav>
+      <h1>
+        <a id="logo" href="#!">e-Learning</a>
+      </h1>
+      <ul>
+        <li>
+          <a href="#!">About Us</a>
+        </li>
+        <li>
+          <a href="#!">Courses</a>
+        </li>
+        <li>
+          <a href="#!">Contact Us</a>
+        </li>
+      </ul>
+    </StyledNav>
+  );
+};
+
+const StyledNav = styled.nav`
+  min-height: 10vh;
+  display: flex;
+  margin: auto;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1rem 10rem;
+  background-color: #383e47;
+
+  a {
+    text-decoration: none;
+    color: #fff;
+
+  }
+
+  ul {
+    display: flex;
+    list-style: none;
+  }
+
+  #logo {
+    font-weight: lighter;
+    font-size: 1.5rem;
+  }
+
+  li {
+    padding-left: 10rem;
+    position: relative;
+  }
+`;
+
+export default Nav;
