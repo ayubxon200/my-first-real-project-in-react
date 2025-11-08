0476089cd3dc4bd790d32384b427f0f0d710fa2e
ayubxon <ayubxonqosimov60@gmail.com>
2025-11-08 19:46:48 +0500

    I push main

--- src/App.js
+++ src/App.js
@@ -4,13 +4,23 @@ import GlobalStyle from "./components/GlobalStyle";
 // import components
 import AboutUs from "./pages/AboutUs";
 import Nav from "./components/Nav";
+// Pages
+import Courses from "./pages/Courses";
+import ContactUs from "./pages/ContactUs";
+// React router dom
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
       <Nav />
-      <AboutUs />
+
+      <Routes>
+        <Route path="/" index element={<AboutUs />} />
+        <Route path="/courses" element={<Courses />} />
+        <Route path="/contact" element={<ContactUs />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/Courses.js
@@ -0,0 +1,59 @@
+import React from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+// images
+import react from "../images/react.jpg";
+import redux from "../images/redux.jpg";
+import javaScript from "../images/js.jpg";
+
+const Courses = () => {
+  return (
+    <StyledCourses>
+      <Course>
+        <h2>Interaktiv React</h2>
+        <div className="line"></div>
+        <Link>
+          <img src={react} alt="react" />
+        </Link>
+      </Course>
+      <Course>
+        <h2>Interaktiv Redux</h2>
+        <div className="line"></div>
+        <Link>
+          <img src={redux} alt="redux" />
+        </Link>
+      </Course>
+      <Course>
+        <h2>Interaktiv JvaScript</h2>
+        <div className="line"></div>
+        <Link>
+          <img src={javaScript} alt="javaScript" />
+        </Link>
+      </Course>
+    </StyledCourses>
+  );
+};
+
+const StyledCourses = styled.div`
+  min-height: 100vh;
+  h2 {
+    padding: 1rem 0;
+  }
+`;
+
+const Course = styled.div`
+  padding: 10rem;
+  img {
+    width: 90%;
+    height: 70vh;
+    object-fit: cover;
+  }
+
+  .line {
+    height: 0.5rem;
+    background-color: #ccc;
+    margin-bottom: 3rem;
+  }
+`;
+
+export default Courses;
