3e1330e4cf1ac682d62e0bfb156699778d27cf9e
ayubxon200 <neverentere024@gmail.com>
2025-11-09 20:09:35 +0500

    I added framer motion

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -5,14 +5,24 @@ import homeIntro from "../images/home_intro.jpg";
 // import styled from "styled-components";
 
 import { About, Description, Image, Hide } from "../Styles";
+// fremer motion
+import { motion } from "framer-motion";
 
 const AboutSection = () => {
+
+  const titleAnim = {
+    hidden: {opacity: 0},
+    show: {opacity: 1, transition: {duration: 2}},
+  }
+
   return (
     <About>
       <Description>
         <div className="title">
           <Hide>
-            <h2>Be educated so that</h2>
+            <motion.h2 variants={titleAnim} initial="hidden" animate="show" >
+              Be educated so that
+            </motion.h2>
           </Hide>
           <Hide>
             <h2>
